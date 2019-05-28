06cd43e9db684e932d7f3cabcfcbbdb41e47b3a9
brandonPurvis <bcpmax@gmail.com>
2019-05-28 19:17:39 -0400

    return server download link.

--- Azure/scripts/get_latest_mc_server_url.py
+++ Azure/scripts/get_latest_mc_server_url.py
@@ -2,7 +2,7 @@ import json
 import urllib2
 
 
-def get_latest():
+def get_latest_info_url():
     url = 'https://launchermeta.mojang.com/mc/game/version_manifest.json'
     response = urllib2.urlopen(url)
     content = response.read()
@@ -12,5 +12,13 @@ def get_latest():
         if version['id'] == latest:
             return version['url']
 
+
+def get_latest():
+    latest_info_url = get_latest_info_url()
+    response = urllib2.urlopen(latest_info_url)
+    content = response.read()
+    values = json.loads(content)
+    latest_server_download = values['downloads']['server']['url']
+    return latest_server_download
 
 print(get_latest())
